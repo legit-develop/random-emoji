77f13342758f48d65947fd332bfa006c52401219
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-04 09:39:45 +0200

    get html elements and add event listener

---
+++ index.js
@@ -0,0 +1,10 @@
+const btnEl = document.getElementById("btn");
+console.log(btnEl);
+const emojiNameEl = document.getElementById("emoji-name");
+
+btnEl.addEventListener("click", (e) => {
+  /*
+    google.com => emoji api
+    www.emoji-api.com
+  */
+});
